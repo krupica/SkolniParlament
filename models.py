5cfe7e346fa3bf83daa66be8671ee5c0d1e26757
krupica <39834203+krupica@users.noreply.github.com>
2018-06-16 10:53:46 +0200

    templaty hotové až na buttony, urls,views,admin
    make dat call sum function
    nejlépe kdyby ještě předával toho vybraného studenta, nebo nějakej jeho parametr, nejlépe token nice

---
+++ models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from uuid import uuid4
+
+# Create your models here.
+class Student(models.Model):
+    jmeno = models.CharField(max_length=30)
+    prijmeni = models.CharField(max_length=30)
+    trida = models.CharField(max_length=10)
+    email = models.CharField(max_length=70)
+    token = models.CharField(max_length=50, blank=True)
+    voted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return ("{} {} {}".format(self.jmeno, self.prijmeni, self.trida))
+
+
+class Kandidat(models.Model):
+    Student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.Student + '-' + self.votes
+
+
+class Vitezove(models.Model):
+    Kandidat = models.ForeignKey(Kandidat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Kandidat
+
+
+def file_process(file=None):
+
+    print("Tak schvalne : ", file)
+    Student.objects.all().delete()
+
+    with open(file) as f:
+        linky = f.read().split("\n")
+        for line in linky:
+            data_dict = {}
+            neco = line.split(";")
+            try:
+                stud = Student()
+                stud.jmeno = neco[0]
+                stud.prijmeni = neco[1]
+                stud.trida = neco[2]
+                stud.email = neco[3]
+                stud.token = uuid4()
+                stud.save()
+            except IndexError:
+                print("Spatny index")
+        print("Parsovani dokonceno")
+
+
+class DataFile(models.Model):
+    nazev = models.CharField(max_length=50)
+    soubor = models.FileField(upload_to='uploaded_files')
+    konec_kandidovani = models.DateTimeField()
+    konec_hlasovani = models.DateTimeField()
+
+    def __str__(self):
+        return self.nazev
+
+    def save(self, *args, **kwargs):
+        print("Saving this shit")
+
+        super(DataFile, self).save()
+        print("Saved")
+
+        file_process(self.soubor.url)
