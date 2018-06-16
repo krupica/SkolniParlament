e7d0634924feee3ad032f6e0dc094a6d5230ff8b
krupica <39834203+krupica@users.noreply.github.com>
2018-06-16 10:57:53 +0200

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    #/hlasovani/
-    url(r'^$', views.hlasovanitest, name='hlasovanitest'),
-    #/hlasovani/token/
-    url(r'^(?P<Student_token>[-\w]+)/$', views.hlasovani, name='hlasovani'),
-]
